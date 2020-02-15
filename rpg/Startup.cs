a6afc9c5c55ffc5d497e3a001ce11fcb152b8ab8
Guilherme Ortiz <guilherme@guilhermeortiz.net>
2020-02-15 17:47:36 -0300

    Second part completed

--- rpg/Startup.cs
+++ rpg/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using rpg.Services;
 
 namespace rpg
 {
@@ -26,6 +27,7 @@ namespace rpg
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddScoped<ICharacterService, CharacterService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,11 +38,11 @@ namespace rpg
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            //app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
